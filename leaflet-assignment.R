d982a8da0b895252aa7b40f6bcfdf9c223775d0e
Robert Tuck <robtuck@gmail.com>
2017-11-09 10:22:47 -0500

    initial commit

---
+++ leaflet-assignment.R
@@ -0,0 +1,4 @@
+library(leaflet)
+
+wineryCoordinates <- read.csv(file="./yountville_coords.csv", header = TRUE, sep = ",")
+
---
+++ parse.js
@@ -0,0 +1,7 @@
+const data = require('./yelp-wineries-yountville_ca_10_miles.json');
+
+console.log('latitude, longitude')
+
+data.businesses.map(business => {
+  console.log(business.coordinates.latitude + "," + business.coordinates.longitude);
+});
