bbb166b918bfb07bc9c65ca772cf19cf1c06cad2
Robert Tuck <robtuck@gmail.com>
2017-11-09 11:00:40 -0500

    created R markdown to generate web page including leaflet map.  Simple js parsers to grap data from yelp and generate data files

--- leaflet-assignment.R
+++ leaflet-assignment.R
@@ -1,4 +1,17 @@
 library(leaflet)
 
-wineryCoordinates <- read.csv(file="./yountville_coords.csv", header = TRUE, sep = ",")
+wineryCoordinates <- read.csv(file="./yountville_winery_coords.csv", header = TRUE, sep = ",")
+
+wineryYelpSites <- scan(file="./yountville_winery_links.csv", what = character(), sep = "\n")
+
+wineryIcon <- makeIcon(
+  iconUrl = "http://robertctuck.com/leaflet-project/wine_glass.png",
+  iconWidth = 31*215/230, iconHeight = 31,
+  iconAnchorX = 31*215/230/2, iconAnchorY = 16
+)
+
+wineryCoordinates %>%
+  leaflet() %>%
+  addTiles() %>%
+  addMarkers(icon = wineryIcon, popup = wineryYelpSites)
 
--- parse.js
+++ parse.js
@@ -2,6 +2,10 @@ const data = require('./yelp-wineries-yountville_ca_10_miles.json');
 
 console.log('latitude, longitude')
 
+// data.businesses.map(business => {
+//   console.log(business.coordinates.latitude + "," + business.coordinates.longitude);
+// });
+
 data.businesses.map(business => {
-  console.log(business.coordinates.latitude + "," + business.coordinates.longitude);
+  console.log("<a href='"+ business.url + "'>" + business.name + "</a>");
 });
